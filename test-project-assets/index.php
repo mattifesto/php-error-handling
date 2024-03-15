a2766d97e40e62633ea959c35a4c1f02aad21b95
Matt Calkins <matt@mattifesto.com>
2024-03-15 02:55:55 +0000

    update test-project-assets/index.php

--- test-project-assets/index.php
+++ test-project-assets/index.php
@@ -3,6 +3,7 @@
 require 'vendor/autoload.php';
 
 use \Mattifesto\ErrorHandling\ExceptionRenderer;
+use \Mattifesto\ErrorHandling\OpenSearchLogger;
 
 $timezone = new DateTimeZone('America/Los_Angeles');
 $date = new DateTime('now', $timezone);
@@ -20,16 +21,65 @@ $date = new DateTime('now', $timezone);
     <?php
 
     try {
-        throw new Exception("test exception");
+        function1();
     } catch (Throwable $throwable) {
         echo '<pre>';
         echo htmlspecialchars(
             ExceptionRenderer::exceptionToText($throwable)
         );
         echo '</pre>';
+
+        $result = OpenSearchLogger::logThrowable($throwable);
+
+        echo "<pre>result: {$result}</pre>";
     }
 
     ?>
 </body>
 
 </html>
+<?php
+
+/**
+ * @return void
+ */
+function function1(): void
+{
+    function2();
+}
+
+
+
+/**
+ * @return void
+ */
+function function2(): void
+{
+    try {
+        $randomInt = random_int(0, PHP_INT_MAX);
+
+        throw new InvalidArgumentException(
+            "first exception: {$randomInt}"
+        );
+    } catch (Throwable $throwable) {
+        function2ErrorHandler($throwable);
+    }
+}
+
+
+
+/**
+ * @param Throwable $throwableArgument
+ * @return void
+ */
+function function2ErrorHandler(
+    Throwable $throwableArgument
+): void {
+    $randomInt = random_int(0, PHP_INT_MAX);
+
+    throw new RuntimeException(
+        "an exception occurred in function2(): {$randomInt}",
+        0,
+        $throwableArgument
+    );
+}
