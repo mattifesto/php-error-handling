0538efb07d70b3f78873ff4b25fa69c0a231631f
Matt Calkins <matt@mattifesto.com>
2024-03-15 02:55:18 +0000

    update src/Mattifesto/ErrorHandling/ExceptionRenderer/ExceptionRenderer.php

--- src/Mattifesto/ErrorHandling/ExceptionRenderer/ExceptionRenderer.php
+++ src/Mattifesto/ErrorHandling/ExceptionRenderer/ExceptionRenderer.php
@@ -4,41 +4,88 @@ namespace Mattifesto\ErrorHandling;
 
 use Throwable;
 
-final class
-ExceptionRenderer
+final class ExceptionRenderer
 {
     /**
      * @return string
      */
-    static function
-    exceptionToText(
+    public static function exceptionToText(
+        Throwable $outermostThrowableArgument
+    ): string {
+        $chronilogicalArrayOfThrowables = array();
+        $chronilogicalArrayOfExceptionTexts = array();
+
+        $currentThrowable = $outermostThrowableArgument;
+
+        while ($currentThrowable !== null) {
+            array_unshift(
+                $chronilogicalArrayOfThrowables,
+                $currentThrowable
+            );
+
+            $currentThrowable = $currentThrowable->getPrevious();
+        }
+
+        $errorIndex = 0;
+
+        foreach ($chronilogicalArrayOfThrowables as $currentThrowable) {
+            $errorText = ExceptionRenderer::singleThrowableToText(
+                $currentThrowable
+            );
+
+            $errorText = <<<EOT
+            ---------- error index {$errorIndex} ----------
+
+            {$errorText}
+            EOT;
+
+            array_push(
+                $chronilogicalArrayOfExceptionTexts,
+                $errorText
+            );
+
+            $errorIndex += 1;
+        }
+
+        $outermostMessage = $outermostThrowableArgument->getMessage();
+
+        $exceptionText =
+        "error: \"{$outermostMessage}\"\n\n\n" .
+        implode(
+            "\n\n\n",
+            $chronilogicalArrayOfExceptionTexts
+        );
+
+        return $exceptionText;
+    }
+
+
+
+    /**
+     * @return string
+     */
+    private static function singleThrowableToText(
         Throwable $throwable
-    ): string
-    {
+    ): string {
         $arrayOfCalls = $throwable->getTrace();
         $arrayOfText = [];
 
-        foreach(
-            $arrayOfCalls as $call
-        ) {
+        foreach ($arrayOfCalls as $call) {
             $file = $call['file'] ?? '';
             $line = $call['line'] ?? '';
             $class = $call['class'] ?? '';
             $function = $call['function'];
 
-            if ($class !== '')
-            {
+            if ($class !== '') {
                 $function = "{$class}::{$function}";
             }
 
             $function = "{$function}()";
 
             $text = <<<EOT
-
             {$file}
             line {$line}
             {$function} was called
-
             EOT;
 
             array_push(
@@ -49,22 +96,16 @@ ExceptionRenderer
 
         $arrayOfText = array_reverse($arrayOfText);
 
-        $message =
-        $throwable->getMessage();
+        $throwableClassName = get_class($throwable);
+        $message = $throwable->getMessage();
+        $file = $throwable->getFile();
+        $line = $throwable->getLine();
 
-        $file =
-        $throwable->getFile();
-
-        $line =
-        $throwable->getLine();
-
-        $text =
-        <<<EOT
-
-        an exception was thrown in the file: {$file}
-        on line: {$line}
-        with the message: {$message}
-
+        $text = <<<EOT
+        {$file}
+        line {$line}
+        a throwable with the class {$throwableClassName} was thrown
+        "{$message}"
         EOT;
 
         array_push(
@@ -72,10 +113,8 @@ ExceptionRenderer
             $text
         );
 
-        $textContent =
-        implode("\n", $arrayOfText);
+        $textContent = implode("\n\n", $arrayOfText);
 
         return $textContent;
     }
-    // exceptionToText()
 }
